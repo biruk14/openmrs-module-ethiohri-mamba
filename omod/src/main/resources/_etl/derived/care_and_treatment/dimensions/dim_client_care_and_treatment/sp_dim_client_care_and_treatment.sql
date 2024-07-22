5b4fa986448e9258f4640c10312c05d3a9bc284c
biruk <bag2161@cumc.columbia.edu>
2024-07-22 15:35:53 +0300

    first commit

--- omod/src/main/resources/_etl/derived/care_and_treatment/dimensions/dim_client_care_and_treatment/sp_dim_client_care_and_treatment.sql
+++
@@ -1,5 +0,0 @@
--- $BEGIN
-CALL sp_dim_client_care_and_treatment_create();
-CALL sp_dim_client_care_and_treatment_insert();
-CALL sp_dim_client_care_and_treatment_update();
--- $END
--- omod/src/main/resources/_etl/derived/care_and_treatment/sp_data_processing_derived_care_and_treatment.sql
+++
@@ -1,4 +0,0 @@
--- $BEGIN
-CALL sp_dim_client_care_and_treatment();
-CALL sp_fact_encounter_care_and_treatment();
--- $END
